5d0c9b2b76e75eeef5152e6b7edc4531547a13e9
Julian B <inujal3@gmail.com>
2019-11-09 16:21:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return "{\"amount\": 35.23, \"creditor\": \"test\"}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
